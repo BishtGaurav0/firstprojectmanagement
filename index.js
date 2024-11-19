af2deef19074df156c01ea7944786e7068af95bc
BishtGaurav0 <bisht1999gaurav@gmail.com>
2024-11-19 22:05:58 +0530

    code to print even number

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const randomArray = [23, 12, 45, 6, 8, 19, 34, 50, 73, 42];
+
+
+for (let i = 0; i < randomArray.length; i++) {
+    if (randomArray[i] % 2 === 0) {
+        console.log(randomArray[i]); // Print the even number
+    }
+}
